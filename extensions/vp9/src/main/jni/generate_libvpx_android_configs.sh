70f0784e254058d8484f2d94825017ac96432b25
Duong Nguyen <nmd99.uet@gmail.com>
2025-12-25 09:23:10 +0700

    update

--- extensions/vp9/src/main/jni/generate_libvpx_android_configs.sh
+++ extensions/vp9/src/main/jni/generate_libvpx_android_configs.sh
@@ -26,8 +26,8 @@ if [ $# -ne 0 ]; then
 fi
 
 # configuration parameters common to all architectures
-common_params="--disable-examples --disable-docs"
-common_params+=" --enable-vp8 --enable-vp9 --disable-vp9-encoder --disable-webm-io"
+common_params="--disable-examples --disable-docs --enable-realtime-only"
+common_params+=" --enable-vp8 --disable-vp9-encoder --disable-webm-io"
 common_params+=" --disable-libyuv --disable-runtime-cpu-detect"
 common_params+=" --enable-external-build"
 common_params+=" --disable-tools"
